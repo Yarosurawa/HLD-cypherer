d3ffc62efc094e492bd5a4da4accb846bceb7750
Yarosurawa <yarosurawa@gmail.com>
2025-06-09 17:18:11 +0300

    idek

--- brainfuckinglyNumbing.js
+++ brainfuckinglyNumbing.js
@@ -2,6 +2,7 @@ const inputEl = document.getElementById('text-input');
 const spaceCBox = document.getElementById('space-cbox')
 const textEl = document.getElementById('text')
 
+
 if (navigator.userAgent.includes("Firefox")) {
   document.body.classList.add("firefox");
 } else if (navigator.userAgent.includes("Chrome")) {
@@ -14,26 +15,64 @@ const heightInput = document.getElementById('height-input');
 const heightInputNum = document.getElementById('height-input-num');
 const widthInput = document.getElementById('width-input');
 const widthInputNum = document.getElementById('width-input-num');
+const borderImg = document.querySelector('.tablet-image');
+const tabletStyleSelect = document.getElementById('tablet-style-select')
+const borderCBox = document.getElementById('border-cbox');
 
+class TabletStyle {
+    constructor(img, x, y, w, h, xMin, yMin, xMax, yMax, mt, ml) {
+        this.img = img
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.xMin = xMin
+        this.xMax = xMax
+        this.yMin = yMin
+        this.yMax = yMax
+        this.mt = mt
+        this.ml = ml
+    }
+}
 
+const tabletStyles = [
+    new TabletStyle("", 0, 0, 0, 0, 1, 1, 16, 16, 0, 0),
+    new TabletStyle("./img/frame1.png", 75, 90, 180, 245, 4, 8, 4, 8, 52.5, 45)
+]
+
+currentTabletStyle = tabletStyleSelect.value
 let tabletWidth = parseInt(widthInput.value);
 let tabletHeight = parseInt(heightInput.value);
 
-let AddedHeight = 90;
-let AddedWidth = 75;
+let AddedWidth = tabletStyles[currentTabletStyle].x;
+let AddedHeight = tabletStyles[currentTabletStyle].y;
 
 [heightInput, heightInputNum, widthInput, widthInputNum].forEach(input => {input.addEventListener('input', ()=>{changeSize()})});
 
 function changeSize() {
+    heightInput.attributes["max"].value = tabletStyles[currentTabletStyle].yMax
+    widthInput.attributes["max"].value = tabletStyles[currentTabletStyle].xMax
+    heightInput.attributes["min"].value = tabletStyles[currentTabletStyle].yMin
+    widthInput.attributes["min"].value = tabletStyles[currentTabletStyle].xMin
+
+    borderImg.width = tabletStyles[currentTabletStyle].w
+    borderImg.height = tabletStyles[currentTabletStyle].h
+    borderImg.src = tabletStyles[currentTabletStyle].img
+
     heightInputNum.value = heightInput.value;
     heightInput.value = heightInputNum.value;
+
     widthInputNum.value = widthInput.value;
     widthInput.value = widthInputNum.value;
     tabletWidth = parseInt(widthInput.value)
     tabletHeight = parseInt(heightInput.value);
+
+    let AddedWidth = tabletStyles[currentTabletStyle].x;
+    let AddedHeight = tabletStyles[currentTabletStyle].y;
     textEl.style.width = `${tabletWidth*25 - 1}px`
     textEl.style.height = `${tabletHeight*20 - 1}px`
-
+    textEl.style.marginLeft = `${tabletStyles[currentTabletStyle].ml}px`;
+    textEl.style.marginTop = `${tabletStyles[currentTabletStyle].mt}px`;
     document.querySelector('.tablet').style.width = `${tabletWidth*25 + AddedWidth}px`
     document.querySelector('.tablet').style.height = `${tabletHeight*20 + AddedHeight}px`
     render();
@@ -64,26 +103,14 @@ spaceCBox.checked ? text = inputEl.value: text = (inputEl.value).replace(/ /g,''
 if(spaceCBox.checked && text.length <= tabletHeight * tabletWidth) { render()}
 else if (!spaceCBox.checked && text.replace(/ /g,'').length <= tabletHeight * tabletWidth) {render()}
 
-const borderImg = document.querySelector('.tablet-image');
-const borderCBox = document.getElementById('border-cbox');
 borderCBox.checked = true;
+borderCBox.addEventListener("change", ()=>{borderCBox.checked === true? document.querySelector('.result').classList.remove("hidden") : document.querySelector('.result').classList.add("hidden")})
 
-borderCBox.addEventListener("change", ()=>{
-    if (borderCBox.checked) {
-        borderImg.style.opacity = 1;
-        textEl.style.marginTop = "52.5px";
-        textEl.style.marginLeft = "45px";
-        AddedHeight = 120;
-        AddedWidth = 90;
-    } else {
-        borderImg.style.opacity = 0;
-        textEl.style.marginTop = "0px";
-        textEl.style.marginLeft = "5px";
-        AddedHeight = 0;
-        AddedWidth = 0;
-    }
-    changeSize();
+tabletStyleSelect.addEventListener("input", ()=>{
+    currentTabletStyle = tabletStyleSelect.value
+    changeSize()
 })
+
 
 render();
 changeSize();
