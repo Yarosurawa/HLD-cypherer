8a3cb9c80f3dd351b2aba540b360415ecc86cce5
Yarosurawa <yarosurawa@gmail.com>
2025-07-12 22:16:36 +0300

    port shennanigans

--- index.js
+++ index.js
@@ -105,6 +105,8 @@ client.on(Events.InteractionCreate, async interaction => {
 
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('alive'));
-app.listen(3000, () => console.log('Web server running'));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
